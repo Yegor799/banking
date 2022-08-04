e890418b0c0258ca0d170ed2ba8d3b7310a67946
Yegor <egoryefim@gmail.com>
2022-08-04 13:40:48 +0300

    added accountDetails component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import AccountDetails from './components/AcountDetails/AccountDetails';
 
 function App() {
   return (
     <div className="container">
-      <Header/>
+      <Header />
+      <AccountDetails/>
     </div>
   );
 }
---
+++ src/components/AcountDetails/AccountDetails.jsx
@@ -0,0 +1,14 @@
+import './AccountDetails.css';
+
+const AccountDetails = () => {
+  return (
+    <div className="account-details">
+      <div className="account">
+        <p>SEPA</p>
+        <p className='details'>Account details</p>
+      </div>
+    </div>
+  )
+}
+
+export default AccountDetails;
