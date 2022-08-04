4f9a0ef22c03a77b9a0d49f223cbd7a23613a146
Yegor <egoryefim@gmail.com>
2022-08-04 20:10:16 +0300

    added content to accountDetails component

--- src/components/AcountDetails/AccountDetails.jsx
+++ src/components/AcountDetails/AccountDetails.jsx
@@ -4,8 +4,34 @@ const AccountDetails = () => {
   return (
     <div className="account-details">
       <div className="account">
-        <p>SEPA</p>
+        <p>litasSepa</p>
         <p className='details'>Account details</p>
+      </div>
+      
+
+      <div className="account-info">
+        <div className="account-info-head">
+          <p className='accout-name'>Account name</p>
+          <p>Currency</p>
+          <p>Current</p>
+          <p>Reserved</p>
+          <p>Available</p>
+        </div>
+
+        <div className="account-info-value">
+          <div className='account-name-value'>
+            <p>3215464214</p>
+            <p>GB04CFZZ00994300000329</p>
+          </div>
+          <p>EUR</p>
+          <p>€100.00</p>
+          <p>€0.00</p>
+          <p>€100.00</p>
+        </div>
+      </div>
+
+      <div className='account-info-bottom'>
+        <p>See all accounts</p>
       </div>
     </div>
   )
