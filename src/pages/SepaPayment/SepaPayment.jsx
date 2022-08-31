6ba196288a559a8caf7610629cccc82d1fdf7327
Yegor <egoryefim@gmail.com>
2022-08-31 19:01:00 +0300

    small changes

--- src/pages/SepaPayment/SepaPayment.jsx
+++ src/pages/SepaPayment/SepaPayment.jsx
@@ -2,6 +2,7 @@ import './SepaPayment.css';
 import { useState, useEffect } from 'react';
 import { useGetAllAccountsQuery } from '../../redux/bankingApi';
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import CircularProgress from '@mui/material/CircularProgress';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
@@ -11,7 +12,8 @@ import Select from '@mui/material/Select';
 const SepaPayment = () => {
 
   const [account, setAccount] = useState('');
-  const [amount, setAmount] = useState('')
+  const [amount, setAmount] = useState(''); 
+
 
   const { data: accounts, isLoading } = useGetAllAccountsQuery();
   const [euroAccounts, setEuroAccounts] = useState(null);
@@ -23,11 +25,14 @@ const SepaPayment = () => {
 
     setEuroAccounts(accounts.filter(account => account.providerCurrency === 'EUR'))
   }, [accounts]);
-  
-  console.log(euroAccounts)
+ 
 
   const handleChange = (e) => {
       setAccount(e.target.value)
+  }
+
+  const handleChangeAmount = (e) => {
+    setAmount(e.target.value);
   }
 
   return (
@@ -49,15 +54,34 @@ const SepaPayment = () => {
               value={account}
               label="From account"
               onChange={handleChange}
+              sx={{ marginBottom: 5 }}
             >
 
               {euroAccounts.map(account => (
                 <MenuItem key={account.number} value={account.number}>
                   <p>{account.providerCurrency}</p>
-                  <p>{account.number}</p>                  
+                  <p>{account.number}</p>
                 </MenuItem>
-              ))}          
+              ))}
             </Select>
+
+            <Box
+              component="form"
+              noValidate
+              autoComplete="off"
+            >
+              <TextField margin="normal" fullWidth label="You send" variant="standard" value={amount} onChange={handleChangeAmount} />
+              <TextField margin="normal" fullWidth label="Name" variant="standard" />
+              <TextField margin="normal" fullWidth label="IBAN" variant="standard" />
+              <TextField margin="normal" fullWidth label="Address" variant="standard" />
+              <TextField margin="normal" fullWidth label="City" variant="standard" />
+              <TextField margin="normal" fullWidth label="State" variant="standard" />
+              <TextField margin="normal" fullWidth label="Postal code" variant="standard" />
+              <TextField margin="normal" fullWidth label="Country" variant="standard" />
+              <TextField margin="normal" fullWidth label="Description" variant="standard" />
+            </Box>
+            
+
           </FormControl>
         </div>
       }
